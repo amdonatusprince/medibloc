72f6b7b04532d1a82a2f63d11154f38627759ec6
amdonatusprince <donatusprince@gmail.com>
2023-11-19 21:09:59 +0100

    contract address updated

--- src/pages/AuthorizedAddresses.jsx
+++ src/pages/AuthorizedAddresses.jsx
@@ -4,12 +4,8 @@ import "../css/styles.css";
 import contractAbi from "../components/contractABI.json";
 import "../css/getPatientRecord.css"
 
-const contractAddress = "0x8084B71fd847053621f36a3A87DDC885f45A467D";
-const abi = contractAbi;
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner(); // Get the signer from the provider
-const contract = new ethers.Contract(contractAddress, abi, signer);
-
+const contractABI = contractAbi;
+const contractAddress = '0x8084B71fd847053621f36a3A87DDC885f45A467D';
 
 const GetAuthorizedAddresses = () => {
 
